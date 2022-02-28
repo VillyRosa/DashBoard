5f4d7a2a5221539df618ce666c27ca2e1f638347
Villy Rosa <villyrosa@hotmail.com>
2022-02-28 01:58:56 -0300

    Adicionada a função de clicar nos botões e correção de bus | 28/02/2022

--- js/script.js
+++ js/script.js
@@ -1,27 +1,39 @@
 var header = window.document.querySelector('header#headerPrincipal')
 var menu = window.document.querySelector('div#menu')
 var home = window.document.querySelector('div#home')
+var carros = window.document.querySelector('div#car')
 var graphics = window.document.querySelector('div#graphics')
 var config = window.document.querySelector('div#config')
 var contador = 0
 var estic = 0
+var section = window.document.querySelector('section#sectionPrincipal')
+var title = window.document.querySelector('div#title')
 
 // Clicar no menu
 function clickMenu() {
     if (contador == 0) {
 
         header.style.width = `200px`
-        menu.innerHTML += `Menu`
-        home.innerHTML += `Home` 
-        graphics.innerHTML += `Gráficos`
-        config.innerHTML += `Configurações`
 
+        setTimeout(function() {
+            menu.innerHTML += `Menu`
+            home.style.width = `200px`
+            home.innerHTML += `Home` 
+            carros.style.width = `200px`
+            carros.innerHTML += `Carros`
+            graphics.style.width = `200px`
+            graphics.innerHTML += `Gráficos`
+            config.style.width = `200px`
+            config.innerHTML += `Configurações`
+        }, 1100)
+        
         estic = 1
         contador = 1
     } else {
         header.style.width = `80px`
         menu.innerHTML = `<i class="fa-solid fa-bars" id="menu-icon"></i>`
         home.innerHTML = `<i class="fa-solid fa-house"></i>`
+        carros.innerHTML = `<i class="fa-solid fa-car"></i>`
         graphics.innerHTML = `<i class="fa-solid fa-chart-line"></i>`
         config.innerHTML = `<i class="fa-solid fa-gear"></i>`
 
@@ -30,12 +42,13 @@ function clickMenu() {
     }
 }
 
-
 // Passar o mouse e tirar nos ícones
 function enterHome() {
     if (estic == 0) {  
-        home.innerHTML += `Home` 
         home.style.width = `200px`
+        setTimeout (function() {
+            home.innerHTML = `<i class="fa-solid fa-house"></i>Home`
+        }, 270) 
     } 
     home.style.backgroundColor = `white`
     home.style.color = `black`
@@ -44,15 +57,38 @@ function enterHome() {
 function leaveHome() {
     if (estic == 0) { 
         home.innerHTML = `<i class="fa-solid fa-house"></i>`
+        home.style.width = `80px`
     } 
     home.style.backgroundColor = ``
     home.style.color = `white`
 }
 
+function enterCar() {
+    if (estic == 0) {  
+        carros.style.width = `200px`
+        setTimeout (function() {
+            carros.innerHTML = `<i class="fa-solid fa-car"></i>Carros`
+        }, 270) 
+    } 
+    carros.style.backgroundColor = `white`
+    carros.style.color = `black`
+}
+
+function leaveCar() {
+    if (estic == 0) { 
+        carros.innerHTML = `<i class="fa-solid fa-car"></i>`
+        carros.style.width = `80px`
+    } 
+    carros.style.backgroundColor = ``
+    carros.style.color = `white`
+}
+
 function enterGraphics() {
     if (estic == 0) {
-        graphics.innerHTML += `Gráficos` 
         graphics.style.width = `200px`
+        setTimeout (function() {
+            graphics.innerHTML = `<i class="fa-solid fa-chart-line"></i>Gráficos`
+        }, 270) 
     }
     graphics.style.backgroundColor = `white`
     graphics.style.color = `black`
@@ -61,15 +97,18 @@ function enterGraphics() {
 function leaveGraphics() {
     if (estic == 0) {
         graphics.innerHTML = `<i class="fa-solid fa-chart-line"></i>`
-    }   
+        graphics.style.width = `80px`
+    } 
     graphics.style.backgroundColor = ``
     graphics.style.color = `white`
 }
 
 function enterConfig() {
     if (estic == 0) {
-        config.innerHTML += `Configurações` 
         config.style.width = `200px`
+        setTimeout(function() {
+            config.innerHTML = `<i class="fa-solid fa-gear"></i>Configurações` 
+        }, 270) 
     }
     config.style.backgroundColor = `white`
     config.style.color = `black` 
@@ -78,7 +117,32 @@ function enterConfig() {
 function leaveConfig() {
     if (estic == 0) {
         config.innerHTML = `<i class="fa-solid fa-gear"></i>`
+        config.style.width = `80px`
     }
     config.style.backgroundColor = ``
     config.style.color = `white`
+}
+
+// Clicar nos ícones do menu
+function clickHome() {
+    title.innerHTML = `<h1>Concessionária</h1><h1>Villy's</h1>`
+    title.style.marginTop = `100px`
+    section.style.backgroundImage = `url(../imgs/carro-de-fundo.jpg)`
+    section.style.backgroundSize = `cover`
+}
+
+function clickCar() {
+    title.innerHTML = `<h1>Carros</h1>`
+    title.style.marginTop = `50px`
+    section.style.background = `#384d68`
+}
+
+function clickGraphics() {
+    title.innerHTML = `<h1>Gráficos</h1>`
+    section.style.background = `#384d68`
+}
+
+function clickConfig() {
+    title.innerHTML = `<h1>Configurações</h1>`
+    section.style.background = `#384d68`
 }
